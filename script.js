79d01c13150141b13d1c265557955450496d1d6a
JJ <jadas3695@gmail.com>
2026-02-03 20:13:43 +0000

    Exercise 2

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-/* CODE GOES HERE */
+let list = [];
+
+function preload(){
+    list = loadJSON("data.json");
+}
+
+
+function setup() {
+  createCanvas(400, 650);
+}
+
+function draw() {
+  background(220);
+//   fill('#fae');
+//   circle(mouseX, mouseY, 100);
+//   fill('skyblue');
+//   rect (70, 20, 50, 50);
+//   fill(0);
+  textAlign("center");
+  textSize(20);
+  text("Canada's Exports", 200, 40);
+
+  Object.values(list).map((item, index) => {
+    fill(item.color);
+    rect(50, index * 30 + 70, item.amount / 180 * 300, 10);
+    textSize(10);
+    textAlign ("left");
+    fill("black");
+    text(item.name, 50, index * 30 + 65);
+
+    textSize(20);
+    textAlign ("right");
+    text(item.icon, 45, index * 30 + 75);
+  });
+
+}
